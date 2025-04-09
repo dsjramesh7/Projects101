13d9621e9d2747dfc809f296cb17bb41377a98cc
dsjramesh7 <rameshdussa07@gmail.com>
2025-04-09 19:44:47 +0530

    practice

---
+++ c_star_rating/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StarRating from "./components/star-rating";
+
+const App = () => {
+  return (
+    <>
+      <StarRating />
+    </>
+  );
+};
+
+export default App;
