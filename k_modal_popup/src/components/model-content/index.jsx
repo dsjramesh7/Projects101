74f9c0fa04908c85e57185122f63f60454d47487
dsjramesh7 <rameshdussa07@gmail.com>
2025-04-15 22:06:40 +0530

    practice

--- k_modal_popup/src/components/model-content/index.jsx
+++ k_modal_popup/src/components/model-content/index.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const ModalContent = ({ id, header, footer, body }) => {
   return (
-    <div id={id || "Modal"}>
-      <div className="header">
-        <span>❌</span>
-        <h1>{header ? header : "Header"}</h1>
+    <div id={id || "Modal"} className="modal">
+      <div className="modal-header">
+        <span className="modal-close">❌</span>
+        <h1 className="modal-title">{header ? header : "Header"}</h1>
       </div>
-      <div className="body">
+      <div className="modal-body">
         {body ? body : <div>This is Our Modal Body</div>}
       </div>
-      <div className="footer">
-        <h1>{footer ? footer : "Footer"}</h1>
+      <div className="modal-footer">
+        <h1 className="modal-footer-text">{footer ? footer : "Footer"}</h1>
       </div>
     </div>
   );
