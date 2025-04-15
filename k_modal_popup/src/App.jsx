29b904009f320c5ddfdc4aa9804e027707d6972c
dsjramesh7 <rameshdussa07@gmail.com>
2025-04-15 15:15:19 +0530

    practice

---
+++ k_modal_popup/src/App.jsx
@@ -0,0 +1,11 @@
+import ModalPopUp from "./components/modal-pop";
+
+const App = () => {
+  return (
+    <div>
+      <ModalPopUp />
+    </div>
+  );
+};
+
+export default App;
---
+++ k_modal_popup/src/components/modal-pop/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ModalPopUp = () => {
+  return <div>ModalPopUp</div>;
+};
+
+export default ModalPopUp;
