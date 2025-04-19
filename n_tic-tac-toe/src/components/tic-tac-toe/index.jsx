f3f33550388fafc7753c89d58b8de64e35eb1cca
dsjramesh7 <rameshdussa07@gmail.com>
2025-04-19 18:16:17 +0530

    practice

--- n_tic-tac-toe/src/components/tic-tac-toe/index.jsx
+++ n_tic-tac-toe/src/components/tic-tac-toe/index.jsx
@@ -1,19 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Square from "../square";
 
 const TicTacToe = () => {
   const [square, setSquare] = useState(Array(9).fill(""));
   const [isXTurn, setIsXTurn] = useState(true);
+  const [status, setStatus] = useState("");
   console.log(square);
+
+  const getWinner = (squares) => {
+    const winningPattern = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2],
+    ];
+
+    for (let i = 0; i < winningPattern.length; i++) {
+      const [x, y, z] = winningPattern[i];
+      if (square[x] && square[x] === square[y] && square[x] === square[z]) {
+        return square[x];
+      }
+    }
+    return null;
+  };
+
+  useEffect(() => {
+    if (!getWinner(square) && square.every((item) => item !== "")) {
+      setStatus(`This is a draw! restart the game`);
+    } else if (getWinner(square)) {
+      setStatus(`The winner is: ${getWinner(square)}`);
+    } else {
+      setStatus(`Next Player is: ${isXTurn ? "X" : "O"}`);
+    }
+  }, [square, isXTurn]);
 
   const handleSquare = (getSquareCurrentIndex) => {
     let cpySquares = [...square];
-    if (cpySquares[getSquareCurrentIndex]) {
+    if (getWinner(cpySquares) || cpySquares[getSquareCurrentIndex]) {
       return;
     }
     cpySquares[getSquareCurrentIndex] = isXTurn ? "X" : "O";
     setSquare(cpySquares);
     setIsXTurn(!isXTurn);
+  };
+
+  const handleButtonRestart = () => {
+    setIsXTurn(true);
+    setSquare(Array(9).fill(""));
   };
   return (
     <div className="flex flex-col gap-2">
@@ -31,6 +68,15 @@ const TicTacToe = () => {
         <Square value={square[6]} onHandleSquareClick={() => handleSquare(6)} />
         <Square value={square[7]} onHandleSquareClick={() => handleSquare(7)} />
         <Square value={square[8]} onHandleSquareClick={() => handleSquare(8)} />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="font-bold text-3xl">{status}</h1>
+        <button
+          className="bg-blue-400 text-white font-semibold px-4 py-2 rounded-xl"
+          onClick={handleButtonRestart}
+        >
+          Restart The Game
+        </button>
       </div>
     </div>
   );
