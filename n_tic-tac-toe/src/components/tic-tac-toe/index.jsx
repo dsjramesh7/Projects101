d7c556cc2cff17d2372e3871971c24547ecfad0c
dsjramesh7 <rameshdussa07@gmail.com>
2025-04-20 09:00:45 +0530

    practice

--- n_tic-tac-toe/src/components/tic-tac-toe/index.jsx
+++ n_tic-tac-toe/src/components/tic-tac-toe/index.jsx
@@ -7,7 +7,7 @@ const TicTacToe = () => {
   const [status, setStatus] = useState("");
   console.log(square);
 
-  const getWinner = (squares) => {
+  const getWinner = (square) => {
     const winningPattern = [
       [0, 1, 2],
       [3, 4, 5],
