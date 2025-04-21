b20ee7dfbca90c4af16cf977729685d62c0ffd35
dsjramesh7 <rameshdussa07@gmail.com>
2025-04-21 15:44:59 +0530

    routing navlink

--- t_food_recipe_app/src/components/navbar/index.jsx
+++ t_food_recipe_app/src/components/navbar/index.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex flex-col justify-between items-center py-8 container mx-auto lg:flex-row gap-5 lg:gap-0">
+      <h1 className="text-2xl font-semibold">
+        <NavLink to={"/"}>
+          Food<span className="text-red-400">Mania</span>
+        </NavLink>
+      </h1>
+      <form action="">
+        <input
+          type="text"
+          name="search"
+          placeholder="Search Food here..."
+          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200 "
+        />
+      </form>
+      <ul className="flex gap-5">
+        <li>
+          <NavLink
+            to={"/"}
+            className="text-black hover:text-red-300 duration-300"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/favourites"}
+            className="text-black hover:text-red-300 duration-300"
+          >
+            Favourites
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
