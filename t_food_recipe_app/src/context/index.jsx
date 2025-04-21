a24e3377a0364c231d27f0f994dd7fed0937ddb4
dsjramesh7 <rameshdussa07@gmail.com>
2025-04-21 20:46:51 +0530

    fetching through api, usecontext

--- t_food_recipe_app/src/context/index.jsx
+++ t_food_recipe_app/src/context/index.jsx
@@ -4,21 +4,43 @@ export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState("");
-  const handleSubmit = async (event) => {
-    event.prevantDefault();
+  const [loading, setLoading] = useState(false);
+  const [recipesList, setRecipesList] = useState([]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
     try {
       const response = await fetch(
         `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
       );
       const result = await response.json();
       console.log(result);
+      if (result?.data?.recipes) {
+        setRecipesList(result?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+      }
     } catch (error) {
       console.log(error);
+      setLoading(false);
+      setSearchParam("");
     }
   };
+
+  if (loading) {
+    return (
+      <h1 className="text-red-400 font-bold text-4xl">Loading wait.....</h1>
+    );
+  }
   return (
     <GlobalContext.Provider
-      value={{ searchParam, setSearchParam, handleSubmit }}
+      value={{
+        searchParam,
+        setSearchParam,
+        handleSubmit,
+        recipesList,
+        loading,
+      }}
     >
       {children}
     </GlobalContext.Provider>
